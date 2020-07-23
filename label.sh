71b2a7aa1199d45715122ee3cb9f17ab20727494
quguanwen <18210690794@163.com>
2020-07-23 21:36:11 +0800

    label

---
+++ label.sh
@@ -0,0 +1,13 @@
+kubectl  label node node01 es/master=true
+kubectl  label node node02 es/master=true
+kubectl  label node node03 es/master=true
+kubectl  label node node01 es/ingest=true
+kubectl  label node node02 es/ingest=true
+kubectl  label node node01 es/data=true
+kubectl  label node node02 es/data=true
+kubectl  label node master01 beta.kubernetes.io/fluentd-ds-ready=true
+kubectl  label node master02 beta.kubernetes.io/fluentd-ds-ready=true
+kubectl  label node master03 beta.kubernetes.io/fluentd-ds-ready=true
+kubectl  label node node01 beta.kubernetes.io/fluentd-ds-ready=true
+kubectl  label node node02 beta.kubernetes.io/fluentd-ds-ready=true
+kubectl  label node node03 beta.kubernetes.io/fluentd-ds-ready=true
